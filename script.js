8969559c5dffdd4b527495816dc314e62ef884f2
Bolliar <rostikposhow@gmail.com>
2019-07-01 22:47:12 +1000

    Summary

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-$(function() {
+$(document).ready(function() {
     
-    $('.nav-toggle-label').click(function() {
-        $(this).toggleClass('nav-active');
-    });
+   
+    if (window.matchMedia('(max-width: 800px)').matches) {
+
+   $('.nav-toggle-label').click(function(event) {
+       event.preventDefault();
+       $('.header-top').toggleClass('menu-active');
+       $(this).toggleClass('nav-active');
+       $('.content').toggleClass('content-active');
+
+   });
     
-})
+        
+        
+    }
+      
+      else {
+          
+       $('.nav-toggle-label').click(function(event) {
+       event.preventDefault();
+       $(this).toggleClass('nav-active');
+       $('nav').toggleClass('nav-pc-active');
+   });
+      }
+    
+
+});
---
+++ scriptscroll.js
@@ -0,0 +1,21 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const progress = document.querySelector('.progress');
+
+
+window.addEventListener('scroll', progressbar);
+
+function progressbar(event) {
+    let windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let per = windowScroll / windowHeight * 100;
+    
+    progress.style.width = per+'%';
+}
